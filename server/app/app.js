3c293fc51352afe029d99a51703b1716028551a1
Neha Yadav <ny663922@gmail.com>
2026-03-23 14:05:39 +0530

    project shet up

---
+++ server/app/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+// import Razorpay from "razorpay";
+// import morgan from "morgan";
+// import cookieParser from "cookie-parser";
+import dbConnect from "../config/dbConfig.js";
+
+dbConnect();
+const app = express();
+// export const instance = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY,
+//   key_secret: process.env.RAZORPAY_SECRET,
+// });
+app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+app.get("/", (req, res) => {
+  res.send("API running 🚀");
+});
+export default app;
